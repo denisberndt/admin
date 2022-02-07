938905ba4123dbef1c688244d99d601943476e34
denisberndt <denisdavid13@yahoo.com>
2022-02-07 13:50:48 +0200

    First Commit

---
+++ consumer.py
@@ -0,0 +1,32 @@
+import pika, json, os, django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "admin.settings")
+django.setup()
+from products.models import Product
+
+params = pika.URLParameters('amqps://lrrgthdt:6bnDhGqcywL-WKJbe5rN0yagP2BNio1V@cow.rmq2.cloudamqp.com/lrrgthdt')
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+channel.queue_declare(queue='admin')
+
+
+def callback(ch, method, properties, body):
+    print('Received in admin')
+    id = json.loads(body)
+    print(id)
+    product = Product.objects.get(id=id)
+    product.likes = product.likes + 1
+    product.save()
+    print('Product likes increased!')
+
+
+channel.basic_consume(queue='admin', on_message_callback=callback, auto_ack=True)
+
+print('Started Consuming')
+
+channel.start_consuming()
+
+channel.close()
